73706fde303158763295e856e6cdbca8514bfe58
beepscore <support@beepscore.com>
2016-02-29 16:03:16 -0800

    In onCreate start audio

--- app/src/main/java/com/beepscore/android/sounddemo/MainActivity.java
+++ app/src/main/java/com/beepscore/android/sounddemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.beepscore.android.sounddemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.media.MediaPlayer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // If rotate device while playing,
+        // activity restarts and phone plays 2 overlapping audios
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.one_small_step_trim);
+        mediaPlayer.start();
     }
 }
