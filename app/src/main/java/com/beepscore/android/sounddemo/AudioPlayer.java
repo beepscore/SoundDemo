99ab1a7701e8e5575b84f2d0444f3255c199b24b
beepscore <support@beepscore.com>
2016-02-29 16:54:57 -0800

    Add class AudioPlayer. It wraps MediaPlayer and prevents creation of multiple players.
    Reference
    https://github.com/beepscore/HelloMoon.git
    
    book "Android Programming", The Big Nerd Ranch Guide
    authors Bill Phillips, Brian Hardy
    http://www.bignerdranch.com/we-write/android-programming
    Chapters 13-15

---
+++ app/src/main/java/com/beepscore/android/sounddemo/AudioPlayer.java
@@ -0,0 +1,42 @@
+package com.beepscore.android.sounddemo;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+/**
+ * Created by stevebaker on 2/29/16.
+ * https://developer.android.com/reference/android/media/MediaPlayer.html
+ */
+public class AudioPlayer {
+    private MediaPlayer mPlayer;
+
+    public void stop() {
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+    public void play(Context context) {
+
+        // prevent creation of multiple media players.
+        if (mPlayer == null) {
+            mPlayer = MediaPlayer.create(context, R.raw.one_small_step_trim);
+        }
+
+        // as soon as playback is done, call stop to release media player
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                stop();
+            }
+        });
+
+        mPlayer.start();
+    }
+
+    public void pause() {
+        mPlayer.pause();
+    }
+
+}
