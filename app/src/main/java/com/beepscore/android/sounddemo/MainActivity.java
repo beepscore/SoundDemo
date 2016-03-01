d342b306a7653366d3929161600d77031de7755d
beepscore <support@beepscore.com>
2016-02-29 16:56:46 -0800

    In MainActivity use AudioPlayer. On buttons set onClickListeners. Now rotating phone does not play multiple overlapping audios.

--- app/src/main/java/com/beepscore/android/sounddemo/MainActivity.java
+++ app/src/main/java/com/beepscore/android/sounddemo/MainActivity.java
@@ -2,18 +2,41 @@ package com.beepscore.android.sounddemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.media.MediaPlayer;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AudioPlayer mPlayer = new AudioPlayer();
+    private Button mPauseButton;
+    private Button mPlayButton;
+    private Button mStopButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View contentView = this.findViewById(android.R.id.content);
 
-        // If rotate device while playing,
-        // activity restarts and phone plays 2 overlapping audios
-        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.one_small_step_trim);
-        mediaPlayer.start();
+        mPauseButton = (Button)contentView.findViewById(R.id.pauseButton);
+        mPauseButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                mPlayer.pause();
+            }
+        });
+
+        mPlayButton = (Button)contentView.findViewById(R.id.playButton);
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                mPlayer.play(getApplicationContext());
+            }
+        });
+
+        mStopButton = (Button)contentView.findViewById(R.id.stopButton);
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                mPlayer.stop();
+            }
+        });
     }
 }
